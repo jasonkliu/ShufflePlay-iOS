c43ab12e95a97c8a6f8eda8ce0b6e3868d78a2be
Jason Liu <jason@jasonkliu.com>
2016-01-01 22:49:33 +0800

    Initial Commit

---
+++ ShufflePlay/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ShufflePlay
+//
+//  Created by Jason Liu on 1/1/16.
+//  Copyright (c) 2016 Jason Liu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
